5b4694ec6f7b50f36a209dde86ca4342f31951d0
Lukas Neubert <serkonda@protonmail.com>
2020-11-07 21:26:38 +0100

    refactor internal handling of the Border struct (#24)
    * some more renaming
    
    * Border --> StyleConfig
    
    - update get_border()
    
    * get_border: update a few missed var renames
    
    * update create_sepline()
    
    * StyleConfig: add default value for each line

--- termtable.v
+++ termtable.v
@@ -42,33 +42,29 @@ pub mut:
 	padding      int = 1
 }
 
-struct Border {
+struct Sepline {
 pub mut:
-	style          Style = .grid
-	top_left       string = '+'
-	top_right      string = '+'
-	top_row_sep    string = '-'
-	bottom_right   string = '+'
-	bottom_left    string = '+'
-	bottom_row_sep string = '-'
-	cross_top      string = '+'
-	cross_right    string = '+'
-	cross_bottom   string = '+'
-	cross_left     string = '+'
-	cross_center   string = '+'
-	head_left      string = '+'
-	head_row_sep   string = '-'
-	head_cross     string = '+'
-	head_right     string = '+'
-	row_sep        string = '-'
-	col_sep        string = '|'
-	fill_padding   bool = true
+	left  string = '+'
+	right string = '+'
+	cross string = '+'
+	sep   string = '-'
+}
+
+struct StyleConfig {
+pub mut:
+	style        Style = .grid
+	topline      Sepline = Sepline{}
+	headerline   Sepline = Sepline{}
+	middleline   Sepline = Sepline{}
+	bottomline   Sepline = Sepline{}
+	col_sep      string = '|'
+	fill_padding bool = true
 }
 
 pub fn (t Table) str() string {
-	expanded_data := expand_tabs(t.data)
-	rowdata, coldata := get_row_and_col_data(expanded_data, t.orientation)
-	col_maxes := colmax(coldata)
+	edata := expand_tabs(t.data)
+	rowdata, coldata := get_row_and_col_data(edata, t.orientation)
+	colmaxes := max_column_sizes(coldata)
 	mut rowstrings := []string{}
 	border := get_border(t.style)
 	for i, row in rowdata {
@@ -78,13 +74,13 @@ pub fn (t Table) str() string {
 		} else if t.orientation == .column {
 			styled_row[0] = apply_header_style(row, t.header_style)[0]
 		}
-		rspace := get_row_spaces(row, col_maxes)
+		rspace := get_row_spaces(row, colmaxes)
 		rowstrings << row_to_string(styled_row, rspace, t.align, t.padding, border)
 	}
-	topline := create_sepline(.top, col_maxes, t.padding, border)
-	headline := create_sepline(.header, col_maxes, t.padding, border)
-	sepline := create_sepline(.middle, col_maxes, t.padding, border)
-	bottomline := create_sepline(.bottom, col_maxes, t.padding, border)
+	topline := create_sepline(.top, colmaxes, t.padding, border)
+	headline := create_sepline(.header, colmaxes, t.padding, border)
+	sepline := create_sepline(.middle, colmaxes, t.padding, border)
+	bottomline := create_sepline(.bottom, colmaxes, t.padding, border)
 	mut final_str := topline
 	for i, row_str in rowstrings {
 		final_str += '$row_str\n'
@@ -98,89 +94,97 @@ pub fn (t Table) str() string {
 	return final_str.trim_space()
 }
 
-fn expand_tabs(data [][]string) [][]string {
-	mut retdata := [][]string{}
-	for d in data {
-		retdata << d.map(it.replace('\t', '    '))
+fn expand_tabs(raw_data [][]string) [][]string {
+	mut edata := [][]string{}
+	for d in raw_data {
+		edata << d.map(it.replace('\t', '    '))
 	}
-	return retdata
+	return edata
 }
 
 fn get_row_and_col_data(data [][]string, orient Orientation) ([][]string, [][]string) {
-	mut otherdata := [][]string{}
-	other_count := data[0].len
-	for i in 0 .. other_count {
+	mut other_data := [][]string{}
+	for i in 0 .. data[0].len {
 		mut od := []string{}
 		for d in data {
 			od << d[i]
 		}
-		otherdata << od
+		other_data << od
 	}
 	if orient == .row {
-		return data, otherdata
+		return data, other_data
 	} else {
-		return otherdata, data
+		return other_data, data
 	}
 }
 
-fn colmax(columns [][]string) []int {
-	mut col_maxes := []int{len: columns.len, init: 0}
+fn max_column_sizes(columns [][]string) []int {
+	mut colmaxes := []int{len: columns.len, init: 0}
 	for i, col in columns {
 		for c in col {
-			if c.len > col_maxes[i] {
-				col_maxes[i] = c.len
+			if c.len > colmaxes[i] {
+				colmaxes[i] = c.len
 			}
 		}
 	}
-	return col_maxes
+	return colmaxes
 }
 
-fn get_border(style Style) Border {
-	mut b := Border{
+fn get_border(style Style) StyleConfig {
+	mut sc := StyleConfig{
 		style: style
 	}
 	match style {
 		.grid {}
 		.plain {
-			b.col_sep = ''
+			sc.col_sep = ''
 		}
 		.simple {
-			b.col_sep = ''
-			b.head_left = ''
-			b.head_right = ''
-			b.head_cross = ''
-			b.fill_padding = false
+			sc.col_sep = ''
+			sc.headerline = Sepline{
+				left: ''
+				right: ''
+				cross: ''
+			}
+			sc.fill_padding = false
 		}
 		.pretty {}
 		.github {
-			b.head_cross = '|'
-			b.head_left = '|'
-			b.head_right = '|'
-			b.cross_left = '|'
-			b.cross_center = '|'
-			b.cross_right = '|'
+			sc.headerline = Sepline{
+				left: '|'
+				right: '|'
+				cross: '|'
+			}
 		}
 		.fancy_grid {
-			b.top_left = '╒'
-			b.top_right = '╕'
-			b.top_row_sep = '═'
-			b.bottom_right = '╛'
-			b.bottom_left = '╘'
-			b.bottom_row_sep = '═'
-			b.cross_top = '╤'
-			b.cross_right = '┤'
-			b.cross_bottom = '╧'
-			b.cross_left = '├'
-			b.cross_center = '┼'
-			b.head_left = '╞'
-			b.head_row_sep = '═'
-			b.head_cross = '╪'
-			b.head_right = '╡'
-			b.row_sep = '─'
-			b.col_sep = '│'
+			sc.topline = Sepline{
+				left: '╒'
+				right: '╕'
+				cross: '╤'
+				sep: '═'
+			}
+			sc.headerline = Sepline{
+				left: '╞'
+				right: '╡'
+				cross: '╪'
+				sep: '═'
+			}
+			sc.middleline = Sepline{
+				left: '├'
+				right: '┤'
+				cross: '┼'
+				sep: '─'
+			}
+			sc.bottomline = Sepline{
+				left: '╘'
+				right: '╛'
+				cross: '╧'
+				sep: '═'
+			}
+			sc.col_sep = '│'
 		}
 	}
-	return b
+	return sc
 }
 
 fn apply_header_style(row []string, style HeaderStyle) []string {
@@ -198,14 +202,14 @@ fn get_row_spaces(row []string, col_sizes []int) []int {
 	return rspace
 }
 
-fn row_to_string(row []string, rspace []int, align Alignment, padding int, b Border) string {
+fn row_to_string(row []string, rspace []int, align Alignment, padding int, sc StyleConfig) string {
 	mut final_row := row.clone()
 	pad := ' '.repeat(padding)
-	mut rstr := b.col_sep + pad
+	mut rstr := sc.col_sep + pad
 	for i, cell in final_row {
 		sl, sr := cell_space(rspace[i], align)
 		rstr += ' '.repeat(sl) + cell + ' '.repeat(sr)
-		rstr += pad + b.col_sep + pad
+		rstr += pad + sc.col_sep + pad
 	}
 	return rstr.trim_space()
 }
@@ -226,57 +230,39 @@ fn cell_space(total_space int, align Alignment) (int, int) {
 	}
 }
 
-fn create_sepline(pos SeplinePos, col_sizes []int, pad int, b Border) string {
-	if b.style == .plain {
+fn create_sepline(pos SeplinePos, col_sizes []int, pad int, sc StyleConfig) string {
+	if sc.style == .plain {
 		return ''
 	}
-	if b.style == .simple && pos != .header {
+	if sc.style == .simple && pos != .header {
 		return ''
 	}
-	if b.style == .pretty && pos == .middle {
+	if sc.style == .pretty && pos == .middle {
 		return ''
 	}
-	if b.style == .github && pos != .header {
+	if sc.style == .github && pos != .header {
 		return ''
 	}
 	padding := pad * 2
-	line_start := match pos {
-		.top { b.top_left }
-		.header { b.head_left }
-		.middle { b.cross_left }
-		.bottom { b.bottom_left }
+	sl_cfg := match pos {
+		.top { sc.topline }
+		.header { sc.headerline }
+		.middle { sc.middleline }
+		.bottom { sc.bottomline }
 	}
-	cross := match pos {
-		.top { b.cross_top }
-		.header { b.head_cross }
-		.middle { b.cross_center }
-		.bottom { b.cross_bottom }
-	}
-	line_end := match pos {
-		.top { b.top_right }
-		.header { b.head_right }
-		.middle { b.cross_right }
-		.bottom { b.bottom_right }
-	}
-	rsep := match pos {
-		.top { b.top_row_sep }
-		.header { b.head_row_sep }
-		.middle { b.row_sep }
-		.bottom { b.bottom_row_sep }
-	}
-	mut line := line_start
+	mut line := sl_cfg.left
 	for i, cs in col_sizes {
-		if b.fill_padding {
-			line += rsep.repeat(cs + padding)
+		if sc.fill_padding {
+			line += sl_cfg.sep.repeat(cs + padding)
 		} else {
-			line += rsep.repeat(cs)
+			line += sl_cfg.sep.repeat(cs)
 			line += ' '.repeat(padding)
 		}
 		if i < col_sizes.len - 1 {
-			line += cross
+			line += sl_cfg.cross
 		}
 	}
-	line += line_end
+	line += sl_cfg.right
 	line = line.trim_space()
 	if pos != .bottom {
 		line += '\n'
